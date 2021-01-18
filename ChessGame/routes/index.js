98edb81e144fc5c56e5c4809726c32558374f3ce
Niek Brouwer <niek.brouwer@outlook.com>
2021-01-18 18:57:21 +0100

    change routes

--- ChessGame/routes/index.js
+++ ChessGame/routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
-var app = express();
+var router = express.Router();
 
-module.exports = function(app){
-  app.get("/", function(req, res) {
-    res.sendFile("splashScreen.html", { root: "./public" });
-  });
+// module.exports = function(app){
+router.get("/splashScreen", function(req, res) {
+  res.sendFile("splashScreen.html", { root: "./public" });
+});
 
   /* Pressing the 'PLAY' button, returns this page */
-  app.get("/play", function(req, res) {
-    res.sendFile("game.html", { root: "./public" });
-  });
-}
+router.get("/play", function(req, res) {
+  res.sendFile("game.html", { root: "./public" });
+});
+
+module.exports = router;
